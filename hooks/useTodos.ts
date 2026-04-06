87fcc575d5532f8fa07521d6b364dc585ca78937
publicjoe <publicjoe@gmail.com>
2026-04-06 13:22:08 +0100

    feat: implement full-featured todo application with local storage persistence and CRUD functionality

---
+++ hooks/useTodos.ts
@@ -0,0 +1,50 @@
+import { useState, useEffect } from "react";
+import { Todo } from "@/public/types/todo";
+import { dummyData } from "@/public/data/todos";
+
+
+export default function useTodos() {
+const [todos, setTodos] = useState(() => {
+    const savedTodos: Todo[] = JSON.parse(localStorage.getItem("todos") || "[]");
+    return savedTodos.length > 0 ? savedTodos : dummyData;
+  });
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  function handleCompletedChange(id: number, completed: boolean) {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id ? { ...todo, completed } : todo
+      )
+    );
+  }
+
+  function handleAddTodo(title: string) {
+    setTodos((prevTodos) => [
+      {
+        id: Date.now(),
+        title,
+        completed: false,
+      },
+      ...prevTodos
+    ]);
+  }
+
+  function handleDeleteTodo(id: number) {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  }
+
+  function handleDeleteAllCompleted() {
+    setTodos((prevTodos) => prevTodos.filter((todo) => !todo.completed));
+  }
+
+  return {
+    todos,
+    handleAddTodo,
+    handleCompletedChange,
+    handleDeleteTodo,
+    handleDeleteAllCompleted,
+  };
+}
